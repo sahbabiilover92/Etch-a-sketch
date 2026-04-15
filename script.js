1fc86a508071717e6d424d36a3785e24e27141f1
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-04-15 14:50:56 -0400

    adjusted userFlexBasis formula and adjusted width and height of container in order to combat wrapping issues for 4 by 4 all the way to 100 by 100.

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const container = document.getElementById("container");
+const button = document.getElementById("btn");
 
-promptNumber=50
+let promptNumber=16;
 
+button.addEventListener("onclick",()=>{
+    let value=prompt("What dimensions?");
+        if (!Number.isInteger(value) && val <= 0 ){
+
+            alert ("try again. make sure you are entering a positive whole number");
+        } else {
+            promptNumber = value;
+        }
+})
+//number recieved from user via prompt
+
+
+//the eventual total number of divs/squares within container
 userInput= (promptNumber)**2;
 
 console.log(userInput);
 
-userFlexBasis= (1000/promptNumber - 0.1);
+/*Bro so originally I had a calculated flex-basis in the styles.css file specifically for 16 by 16.
+This equation, instead of using percentages, uses pixels. and works for any number.
+I went and adjusted the width and height to 1005 in the css file in order to combat wrapping issues.*/
+userFlexBasis= (1000/promptNumber);
 
 console.log(userFlexBasis)
+
+/*each iteration creates a new div. 
+it only loops up to the total amount of desired squares or "blank by blank". 
+Also styles and contains mouseover event.*/
 for (let i=0; i<userInput; i++){
     const squareDiv = document.createElement("div");
     squareDiv.className="squareDiv";
