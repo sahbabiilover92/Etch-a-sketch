c73afbb04f89cab691283689477526619e89d753
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-04-15 14:14:59 -0400

    adjusted flex basis for squareDiv so that no matter the input, each row would end based on the input without wrapping prior.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const container = document.getElementById("container");
 
-userInput= (16)**2;
+promptNumber=50
+
+userInput= (promptNumber)**2;
 
 console.log(userInput);
 
+userFlexBasis= (1000/promptNumber - 0.1);
+
+console.log(userFlexBasis)
 for (let i=0; i<userInput; i++){
     const squareDiv = document.createElement("div");
     squareDiv.className="squareDiv";
     container.appendChild(squareDiv);
+    squareDiv.style.flex = `1 1 ${userFlexBasis}px`;
+    squareDiv.style.backgroundColor = "blue";
 
     squareDiv.addEventListener("mouseover", ()=>{
         squareDiv.style.backgroundColor = "green";
+    
     });
 };
 
