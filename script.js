f9d7d22df6d1b9496e9a1b019e5b7f1ae5c34d0d
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-04-15 15:17:59 -0400

    assigned square generation to a function. this allows the function to be called anytime we click the button. Also created conditionals for which inputs can be accepted within the button event.

--- script.js
+++ script.js
@@ -3,43 +3,48 @@ const button = document.getElementById("btn");
 
 let promptNumber=16;
 
-button.addEventListener("onclick",()=>{
-    let value=prompt("What dimensions?");
-        if (!Number.isInteger(value) && val <= 0 ){
-
-            alert ("try again. make sure you are entering a positive whole number");
-        } else {
-            promptNumber = value;
-        }
-})
-//number recieved from user via prompt
-
-
-//the eventual total number of divs/squares within container
-userInput= (promptNumber)**2;
-
-console.log(userInput);
-
-/*Bro so originally I had a calculated flex-basis in the styles.css file specifically for 16 by 16.
-This equation, instead of using percentages, uses pixels. and works for any number.
-I went and adjusted the width and height to 1005 in the css file in order to combat wrapping issues.*/
-userFlexBasis= (1000/promptNumber);
-
-console.log(userFlexBasis)
-
 /*each iteration creates a new div. 
 it only loops up to the total amount of desired squares or "blank by blank". 
 Also styles and contains mouseover event.*/
-for (let i=0; i<userInput; i++){
-    const squareDiv = document.createElement("div");
-    squareDiv.className="squareDiv";
-    container.appendChild(squareDiv);
-    squareDiv.style.flex = `1 1 ${userFlexBasis}px`;
-    squareDiv.style.backgroundColor = "blue";
+let generateSquares = function(){
+        
+    //the eventual total number of divs/squares within container
+    let userInput= (promptNumber)**2;
 
-    squareDiv.addEventListener("mouseover", ()=>{
-        squareDiv.style.backgroundColor = "green";
-    
+    console.log(userInput);
+
+    /*Bro so originally I had a calculated flex-basis in the styles.css file specifically for 16 by 16.
+    This equation, instead of using percentages, uses pixels. and works for any number.
+    I went and adjusted the width and height to 1005 in the css file in order to combat wrapping issues.*/
+    let userFlexBasis= (1000/promptNumber);
+
+    console.log(userFlexBasis)
+
+        for (let i=0; i<userInput; i++){
+        const squareDiv = document.createElement("div");
+        squareDiv.className="squareDiv";
+        container.appendChild(squareDiv);
+        squareDiv.style.flex = `1 1 ${userFlexBasis}px`;
+        squareDiv.style.backgroundColor = "blue";
+
+        squareDiv.addEventListener("mouseover", ()=>{
+            squareDiv.style.backgroundColor = "green";
+        
     });
-};
+}; 
+}; generateSquares();
+//
+button.addEventListener("click",()=>{
+    let value = Number(prompt("What dimensions?"));
+    if (!Number.isInteger(value) || value <= 0){
+        alert("try again (make sure it's a positive whole number)");
+    } else {
+        promptNumber=value;
+        return generateSquares();
+    }
+});
+//number recieved from user via prompt
+
+
+
 
