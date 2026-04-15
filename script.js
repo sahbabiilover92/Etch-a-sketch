4099c9e92c14445acf4d93a2b4790c0fb6ad749d
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-04-15 12:07:46 -0400

    increased number of squaredivs and added wrapping.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById("container");
 
-for (let i=0; i<16; i++){
+for (let i=0; i<256; i++){
     const squareDiv = document.createElement("div");
     squareDiv.className="squareDiv";
     container.appendChild(squareDiv);
