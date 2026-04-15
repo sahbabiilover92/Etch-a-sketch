51ba3b021ce82a0a9500734132e7bbc650556147
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-04-15 12:05:24 -0400

    create function that allows for multiple squaredivs to be made

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const squareDiv = document.createElement("div");
 const container = document.getElementById("container");
-squareDiv.id = "squareDiv";
-container.appendChild(squareDiv);
+
+for (let i=0; i<16; i++){
+    const squareDiv = document.createElement("div");
+    squareDiv.className="squareDiv";
+    container.appendChild(squareDiv);
+};
